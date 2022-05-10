7605c3a08ad0e59aaec34e86cb16bfd1fb415668
Gavin <7797864896@K12.LCS>
2022-05-10 13:48:26 -0400

    Display Breed names on app component using vue for loop

---
+++ src/components/App.vue
@@ -0,0 +1,23 @@
+
+<template>
+  <div :key="dog.index" v-for="dog in dogs">
+    <h1 class="text-2xl">{{ dog.breed }}</h1>
+  </div>
+</template>
+
+<script>
+// import DisplayBreeds from './DisplayBreeds.vue';
+import dogs from '../dogs.js';
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      dogs: dogs
+    }
+  }
+}
+
+</script>
+
+
---
+++ src/dogs.js
@@ -0,0 +1,12 @@
+const dogs = [
+    {breed: "Barbet", photo: '../assets/barbet.jpg', isHypoallergenic: true}, 
+    {breed: "Chihuahua", photo: '../assets/chihuahua.jpg', isHypoallergenic: false},
+    {breed: "German Shepherd", photo: '../assets/german-shepherd.jpg', isHypoallergenic: false},
+    {breed: "Golden Retriever", photo: '../assets/golden-retriever.jpg', isHypoallergenic: false},
+    {breed: "Husky", photo: '../assets/husky.jpg', isHypoallergenic: false},
+    {breed: "Poodle", photo: '../assets/poodle.jpg', isHypoallergenic: true},
+    {breed: "Schnauzer", photo: '../assets/schnauzer.jpg', isHypoallergenic: true},
+    {breed: "Yorkie", photo: '../assets/yorkie.jpg', isHypoallergenic: true},
+];
+
+export default dogs;
